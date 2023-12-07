88405b764446d0794f85f31b73e62f19a1c3138f
Sebastien Tardif <Sebastien.Tardif@verituity.com>
2023-12-06 19:21:15 -0500

    Working code

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # Return an indefinite article "a" or "an" for the animal name.
+from typing import Any
+
+
 def article(name):
     if name[0].lower() in "aeiou":
         return "an"
@@ -7,7 +10,6 @@ def article(name):
 
 
 from enum import Enum
-from functools import total_ordering
 
 
 class Truth(Enum):
@@ -56,11 +58,166 @@ def parse_operator(operator):
     return Operator.NONE, Precedence.NONE
 
 
+# Represents a Boolean expression.
+class BoolExpression:
+    # Parse the expression.
+    def __init__(self, expression):
+        # Remove whitespace and save the expression string.
+        expression = expression.strip()
+        self.expression = expression
+
+        # The goal is to set these properties.
+        self.operator = None
+        self.operand1 = None
+        self.operand2 = None
+
+        # Find the lowest precedence operator that
+        # is not inside parentheses.
+        # Initially we have no operator.
+        best_prec = Precedence.NONE
+        best_i = -1
+        best_op = Operator.NONE
+
+        # Loop through the expression.
+        open_parens = 0
+        for i in range(len(expression)):
+            ch = expression[i]
+            if ch == '(':
+                open_parens += 1
+            elif ch == ')':
+                open_parens -= 1
+                if open_parens < 0:
+                    raise Exception(f'Extra close paren ")" in "{expression}" at position {i}')
+            elif open_parens == 0:
+                # No parens are currently open.
+                # If this is an operator, see if it is an improvement.
+                new_op, new_prec = parse_operator(ch)
+                if new_prec < best_prec:
+                    best_prec = new_prec
+                    best_i = i
+                    best_op = new_op
+
+        # See if all parens were closed.
+        if open_parens > 0:
+            raise Exception(f'Unclosed open paren "(" in "{expression}"')
+
+        # If we still have no operator, then either:
+        #    - The expression has the form "(subexpression)"
+        #    - The expression is a question like 'breathes water'
+        if best_i < 0:
+            # See if it's (subexpression).
+            if expression[0] == '(':
+                # Remove the parentheses.
+                expression = expression.strip()[1:-1]
+
+                # Evaluate whatever was inside the parentheses.
+                self.operator = Operator.PAREN
+                self.operand1 = BoolExpression(expression)
+                self.operand2 = None
+                return
+            else:
+                # It's a question.
+                self.operator = Operator.FACT
+                self.operand1 = expression
+                self.operand2 = None
+                return
+
+        # We have an operator.
+        self.operator = best_op
+
+        # Split the expression at the operator.
+        if best_op == Operator.NOT:
+            # NOT.
+            # Remove the !.
+            self.operand1 = BoolExpression(expression[1:])
+        else:
+            # AND and OR.
+            # Split at the operator (removing the operator).
+            self.operand1 = BoolExpression(expression[:best_i])
+            self.operand2 = BoolExpression(expression[best_i + 1:])
+
+    # Return a set holding the facts that this expression needs for evaluation.
+    def list_facts(self):
+        # FACT
+        if self.operator == Operator.FACT:
+            return {self.operand1}
+
+        # PAREN or NOT
+        if self.operator == Operator.PAREN or self.operator == Operator.NOT:
+            return self.operand1.list_facts()
+
+        # AND or OR
+        if self.operator == Operator.AND or self.operator == Operator.OR:
+            return self.operand1.list_facts().union(self.operand2.list_facts())
+
+    # Evaluate the expression given the current facts.
+    def evaluate(self, facts):
+        # FACT
+        if self.operator == Operator.FACT:
+            # Just return whatever is in facts.
+            return facts[self.operand1]
+
+        # PAREN
+        if self.operator == Operator.PAREN:
+            # Evaluate operand1 and return its value.
+            return self.operand1.evaluate(facts)
+
+        # NOT
+        if self.operator == Operator.NOT:
+            # If the value is TRUE or FALSE, negate it.
+            truth1 = self.operand1.evaluate(facts)
+            if truth1 == Truth.TRUE:
+                return Truth.FALSE
+            if truth1 == Truth.FALSE:
+                return Truth.TRUE
+            return Truth.UNKNOWN
+
+        # AND
+        if self.operator == Operator.AND:
+            # If both values are TRUE, return TRUE.
+            # If either value is FALSE, return FALSE.
+            # Otherwise, return UNKNOWN.
+            truth1 = self.operand1.evaluate(facts)
+            truth2 = self.operand2.evaluate(facts)
+            if truth1 == Truth.TRUE and truth2 == Truth.TRUE:
+                return Truth.TRUE
+            if truth1 == Truth.FALSE or truth2 == Truth.FALSE:
+                return Truth.FALSE
+            return Truth.UNKNOWN
+
+        # OR
+        if self.operator == Operator.OR:
+            # If either value is TRUE, return TRUE.
+            # If both values are FALSE, return FALSE.
+            # Otherwise, return UNKNOWN.
+            truth1 = self.operand1.evaluate(facts)
+            truth2 = self.operand2.evaluate(facts)
+            if truth1 == Truth.TRUE or truth2 == Truth.TRUE:
+                return Truth.TRUE
+            if truth1 == Truth.FALSE and truth2 == Truth.FALSE:
+                return Truth.FALSE
+            return Truth.UNKNOWN
+
+
 # Represents a complete rule of the form 'antecedent -> consequent'.
 # The antecedent is a BoolExpression and the consequent is a fact.
 class Rule:
+    antecedent: BoolExpression
+    is_goal: bool
+    is_negated: bool
+    rule_string: str
+    antecedent_truth: Truth
+    antecedent_string: str
+    consequent_string: str
+
     def __init__(self, rule_string):
-        ...
+        self.antecedent_truth = Truth.UNKNOWN
+        self.rule_string = rule_string.strip()
+        self.antecedent_string, self.consequent_string = rule_string.strip().split("->")
+        self.is_negated = '!' in self.consequent_string
+        self.is_goal = '$' in self.consequent_string
+        self.consequent = self.consequent_string.replace('!', '').replace('$', '').strip()
+        self.antecedent = BoolExpression(self.antecedent_string)
 
     # Return the truth of the rule's consequent.
     def truth(self, facts):
@@ -73,31 +230,65 @@ class Rule:
 
     # Evaluate the antecedent. If it's now TRUE,
     # then set the consequent's value.
-    def evaluate(self, facts, explain):
-        ...
+    def evaluate(self, facts, explain: bool):
+        # If the consequent's truth is known, return it.
+        if facts[self.consequent] != Truth.UNKNOWN:
+            return facts[self.consequent]
+
+        # If we have previously set the antecedent's truth,
+        # return the consequent's current truth.
+        if self.antecedent_truth != Truth.UNKNOWN:
+            return facts[self.consequent]  # has to be UNKNOWN
+
+        # Otherwise evaluate the antecedent.
+        truth = self.antecedent.evaluate(facts)
+
+        # See if the antecedent is now TRUE.
+        if truth == Truth.TRUE:
+            # It's true. Set the consequent's truth value.
+            # Negate if necessary.
+            if self.is_negated:
+                facts[self.consequent] = Truth.FALSE
+            else:
+                facts[self.consequent] = Truth.TRUE
+
+            # If we should explain, do so.
+            if explain:
+                print(self.explain(facts))
+
+        # Return the result in the fact's dictionary.
+        return facts[self.consequent]
 
     # Return a string that explains the reason why this rule's value changed.
     def explain(self, facts):
-        ...
+        if self.is_goal:
+            result = f'    GOAL {self.consequent} is {facts[self.consequent]} because:'
+        else:
+            result = f'    RULE {self.consequent} is {facts[self.consequent]} because:'
+        result += f'\n        it follows from '
 
+        # If we're negated, add "not."
+        if self.is_negated:
+            result += 'NOT '
 
-# Represents a Boolean expression.
-class BoolExpression:
-    # Parse the expression.
-    def __init__(self, expression):
-        ...
+        # Add the antecedent expression.
+        result += f'"{self.antecedent_string}" and:'
 
-    # Return a set holding the facts that this expression needs for evaluation.
-    def list_facts(self):
-        ...
-
-    # Evaluate the expression given the current facts.
-    def evaluate(self, facts):
-        ...
+        # Add the facts that go into the evaluation.
+        for fact_name in self.antecedent.list_facts():
+            # Only list facts that are not UNKNOWN.
+            if facts[fact_name] != Truth.UNKNOWN:
+                result += f'\n            {fact_name} = {facts[fact_name]}'
+        return result
 
 
 class Truthifier:
+    facts: dict[Any, Truth]
+    goals_reached: list[Any]
+
     def __init__(self, rule_strings):
+        self.rules = None
+        self.facts = dict[Any, Truth]
         self.load_rules(rule_strings)
         self.load_facts()
         self.load_questions()
@@ -162,7 +353,6 @@ class Truthifier:
         print("    d = Dump data")
         print("    q = Quit")
         print()
-        self.asdf()
 
         # Ask questions.
         for question in self.questions:
@@ -217,9 +407,9 @@ class Truthifier:
                         # and it's not already in goals_reached,
                         # then add it to goals_reached.
                         if (
-                            rule.is_goal
-                            and (truth == Truth.TRUE)
-                            and (rule not in self.goals_reached)
+                                rule.is_goal
+                                and (truth == Truth.TRUE)
+                                and (rule not in self.goals_reached)
                         ):
                             self.goals_reached.append(rule)
 
